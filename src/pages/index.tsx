cb958e6be8d959afda56ab7db3cae32984235302
osmak1234 <tadeashanus0631@gmail.com>
2023-05-20 12:16:49 +0200

    First commit with all npm packages

---
+++ src/pages/index.tsx
@@ -0,0 +1,40 @@
+import { signIn, signOut, useSession } from "next-auth/react";
+import { type NextPage } from "next";
+
+const Home: NextPage = () => {
+  const { data: session, status } = useSession();
+
+  if (status === "loading") {
+    return <main>Loading...</main>;
+  }
+
+  return (
+    <main>
+      <h1>Guestbook</h1>
+      <div>
+        {session ? (
+          <>
+            <p>hi {session.user?.name}</p>
+            <button
+              onClick={() => {
+                signOut().catch(console.log);
+              }}
+            >
+              Logout
+            </button>
+          </>
+        ) : (
+          <button
+            onClick={() => {
+              signIn("discord").catch(console.log);
+            }}
+          >
+            Login with Discord
+          </button>
+        )}
+      </div>
+    </main>
+  );
+};
+
+export default Home;
