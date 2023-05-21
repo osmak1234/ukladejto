21ff53053940c5b8210a82ab586b82c25cea4ef0
osmak1234 <tadeashanus0631@gmail.com>
2023-05-21 15:02:30 +0200

    Files in prisma schema

--- src/pages/test/index.tsx
+++ src/pages/test/index.tsx
@@ -1,5 +1,10 @@
+// ChakraUI
+import { Button, Box, Text } from "@chakra-ui/react";
+
+//upload component
 import { UploadButton } from "@uploadthing/react";
 
+// trp router for the upload endpoint
 import type { OurFileRouter } from "~/server/uploadthing";
 
 // You need to import our styles for the button to look right. Best to import in the root /_app.tsx but this is fine
@@ -7,19 +12,21 @@ import "@uploadthing/react/styles.css";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <UploadButton<OurFileRouter>
-        endpoint="imageUploader"
-        onClientUploadComplete={(res) => {
-          // Do something with the response
-          console.log("Files: ", res);
-          alert("Upload Completed");
-        }}
-        onUploadError={(error: Error) => {
-          // Do something with the error.
-          alert(`ERROR! ${error.message}`);
-        }}
-      />
-    </main>
+    <Box pt={70}>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <UploadButton<OurFileRouter>
+          endpoint="imageUploader"
+          onClientUploadComplete={(res) => {
+            // Do something with the response
+            console.log("Files: ", res);
+            alert("Upload Completed");
+          }}
+          onUploadError={(error: Error) => {
+            // Do something with the error.
+            alert(`ERROR! ${error.message}`);
+          }}
+        />
+      </main>
+    </Box>
   );
 }
