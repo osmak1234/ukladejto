92214cba751a258cd44d43a1c5c708f835413737
osmak1234 <tadeashanus0631@gmail.com>
2023-07-11 10:55:17 +0200

    fixed the flashcard upload
    I messed up with bad async code, so now it's cleaned up and works as intended

--- src/server/api/routers/flashcards.ts
+++ src/server/api/routers/flashcards.ts
@@ -1,3 +1,4 @@
+import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
@@ -6,72 +7,69 @@ export const flashcards = createTRPCRouter({
   newDeck: publicProcedure
     .input(z.object({ name: z.string() }))
     .mutation(({ ctx, input }) => {
-      if (!ctx.session?.user) {
-        throw new Error("not logged in");
-      } else {
-        return ctx.prisma.deck.create({
-          data: {
-            name: input.name,
-            userId: ctx.session.user.id,
-          },
-        });
-      }
-    }),
-  getDecks: publicProcedure.input(z.object({})).query(({ ctx }) => {
-    if (!ctx.session?.user) {
-      throw new Error("not logged in");
-    } else {
-      return ctx.prisma.deck.findMany({
-        where: {
-          userId: ctx.session.user.id,
-        },
+      if (!ctx.session?.user)
+        throw new TRPCError({ code: "UNAUTHORIZED", message: "Not logged in" });
+      return ctx.prisma.deck.create({
+        data: { name: input.name, userId: ctx.session.user.id },
       });
-    }
+    }),
+
+  getDecks: publicProcedure.input(z.object({})).query(({ ctx }) => {
+    if (!ctx.session?.user)
+      throw new TRPCError({ code: "UNAUTHORIZED", message: "Not logged in" });
+    return ctx.prisma.deck.findMany({ where: { userId: ctx.session.user.id } });
   }),
 
   getDeck: publicProcedure
     .input(z.object({ id: z.string() }))
-    .query(({ ctx, input }) => {
-      if (!ctx.session?.user) {
-        throw new Error("not logged in");
-      } else {
-        return ctx.prisma.deck.findFirst({
-          where: {
-            id: input.id,
-            userId: ctx.session.user.id,
-          },
-          include: {
-            FlashCards: true,
-          },
-        });
-      }
+    .query(async ({ ctx, input }) => {
+      if (!ctx.session?.user)
+        throw new TRPCError({ code: "UNAUTHORIZED", message: "Not logged in" });
+      const deck = await ctx.prisma.deck.findFirst({
+        where: { id: input.id, userId: ctx.session.user.id },
+        include: { FlashCards: true },
+      });
+      if (!deck)
+        throw new TRPCError({ code: "NOT_FOUND", message: "Deck not found" });
+      return deck;
     }),
 
   createFlashcards: publicProcedure
     .input(
       z.array(
-        z.object({
-          front: z.string(),
-          back: z.string(),
-          deckId: z.string(),
-        })
+        z.object({ front: z.string(), back: z.string(), deckId: z.string() })
       )
     )
-    .mutation(({ ctx, input }) => {
-      if (!ctx.session?.user?.id) {
-        throw new Error("not logged in");
-      } else {
-        const modifiedInput = input.map((flashcard) => {
-          return {
-            front: flashcard.front,
-            back: flashcard.back,
-            deckId: flashcard.deckId,
-            userId: ctx.session?.user.id ?? "",
-          };
+    .mutation(async ({ ctx, input }) => {
+      if (!ctx.session?.user)
+        throw new TRPCError({ code: "UNAUTHORIZED", message: "Not logged in" });
+      if (!input.length)
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: "No flashcard data provided",
         });
-        return ctx.prisma.flashCard.createMany({
-          data: modifiedInput,
+
+      const existingDeck = await ctx.prisma.deck.findFirst({
+        where: { id: input[0].deckId },
+      });
+      if (existingDeck)
+        throw new TRPCError({
+          code: "BAD_REQUEST",
+          message: "Deck already exists",
         });
-      }
+
+      const newDeck = await ctx.prisma.deck.create({
+        data: { name: "default", userId: ctx.session.user.id },
+      });
+      const flashcardsModified = input.map((item) => ({
+        ...item,
+        deckId: newDeck.id,
+        userId: ctx.session.user.id,
+      }));
+      const flashcardsCreated = await ctx.prisma.flashCard.createMany({
+        data: flashcardsModified,
+      });
+
+      return flashcardsCreated;
     }),
 });
